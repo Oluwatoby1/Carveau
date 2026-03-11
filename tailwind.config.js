933b4e5a56c6e14f1fbdbe425850d43be1d00ad9
Oluwatoby1 <testimonyadio8@gmail.comm>
2026-03-11 13:48:59 +0100

    Initial commit with SEO and links updated

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "769px",
+      menu: "769px",
+      lg: "1200px",
+    },
+    extend: {
+      fontFamily: {
+        montreal: ["NeueMontreal", "sans-serif"],
+      },
+      keyframes: {
+        "move-lines": {
+          "0%": { backgroundPosition: "0 0" },
+          "100%": { backgroundPosition: "0 50px" },
+        },
+      },
+      animation: {
+        "move-vertical": "move-vertical 60s linear infinite",
+        "60s ease-in-out infinite": "",
+      },
+    },
+  },
+  plugins: [],
+};
